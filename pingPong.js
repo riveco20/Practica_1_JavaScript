f6447ec8b422ba12ef4413c7fe4cc858a5886ee8
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-07 18:25:45 -0500

    Creando los objetos barras

--- pingPong.js
+++ pingPong.js
@@ -15,14 +15,45 @@
 
         get elements(){
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
         }
 
      }
+
+     
     
 
 })();
+
+
+(function(){
+self.Bar = function(x,y,whidth,height,board) {
+    
+    this.x = x;
+    this.y = y;
+    this.width=whidth;
+    this.height = height;
+    this.board = board;
+    this.board.bars.push(this);
+    this.kind ="rectangle";
+
+}
+
+
+self.Bar.prototype ={
+    down:function(){
+
+    },
+
+    up:function(){
+
+    }
+}
+
+})();
+
+
 
 (function(){
     self.BoardView = function(canvas,board){
@@ -33,6 +64,27 @@
         this.ctx = canvas.getContext("2d");
 
     }
+
+    self.BoardView.prototype={
+        draw: function(){
+            for (var i = this.board.elements.length-1; i>=0; i--) {
+                var el = this.board.elements[i];
+              
+                draw(this.ctx,el)
+            };
+        }
+    };
+    function draw(ctx,element){
+        if(element!== null && element.hasOwnProperty("kind")){
+        switch(element.kind){
+            case "rectangle":
+            ctx.fillRect(element.x,element.y,element.width,element.height);
+            break;
+            
+        }
+    }
+    }
+
 }())
 
 window.addEventListener("load",main);
@@ -42,6 +94,9 @@ window.addEventListener("load",main);
 function main(){
 
     var board = new Board(800,400);
+    var bar = new Bar(20,100,40,100,board);
+    var bar = new Bar(700,100,40,100,board);
     var canvas = document.getElementById('canvas');
-    var board_view = new BoardView(canvas,board)
+    var boardview = new BoardView(canvas,board)
+    boardview.draw();
 };
