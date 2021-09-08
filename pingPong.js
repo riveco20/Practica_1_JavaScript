178d87dcfb7e9643665aeecadecba18bdc78aa8b
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-07 21:52:01 -0500

    Agregando eventos a las barras en la cordenada y

--- pingPong.js
+++ pingPong.js
@@ -8,7 +8,7 @@
         this.gameOver=false;
         this.bars=[];
         this.ball= null;
-
+        
     }
 
     self.Board.prototype ={
@@ -37,6 +37,7 @@ self.Bar = function(x,y,whidth,height,board) {
     this.board = board;
     this.board.bars.push(this);
     this.kind ="rectangle";
+    this.speed=10;
 
 }
 
@@ -44,10 +45,17 @@ self.Bar = function(x,y,whidth,height,board) {
 self.Bar.prototype ={
     down:function(){
 
+        this.y+= this.speed;
+
     },
 
     up:function(){
+        this.y -= this.speed;
 
+    },
+
+    toString:function(){
+        return "x: " + this.x + "y: "+ this.y;
     }
 }
 
@@ -85,18 +93,40 @@ self.Bar.prototype ={
     }
     }
 
-}())
+})();
 
-window.addEventListener("load",main);
+
+var board = new Board(800,400);
+var bar = new Bar(20,100,40,100,board);
+var bar = new Bar(700,100,40,100,board);
+var canvas = document.getElementById('canvas');
+var boardview = new BoardView(canvas,board)
+
+
+
+document.addEventListener("keydown",function(ev){
+
+    if(ev.keyCode==38){
+        bar.up();
+
+    }
+
+    else if(ev.keyCode==40){
+        bar.down();
+    }
+
+
+    
+
+
+});
+
+
+self.addEventListener("load",main);
 
 
 
 function main(){
 
-    var board = new Board(800,400);
-    var bar = new Bar(20,100,40,100,board);
-    var bar = new Bar(700,100,40,100,board);
-    var canvas = document.getElementById('canvas');
-    var boardview = new BoardView(canvas,board)
     boardview.draw();
 };
