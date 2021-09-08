43042c94e643d991946a8c677a27a127e85c5700
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-07 23:01:22 -0500

    Agregando objeto pelota

--- pingPong.js
+++ pingPong.js
@@ -11,11 +11,30 @@
     self.Board.prototype ={
         get elements(){
             var elements = this.bars;
-            //elements.push(this.ball);
+            elements.push(this.ball);
             return elements;
         }
      }
 })();
+
+(function(){
+
+    self.Ball = function(x,y,radius,board){
+        this.x =x;
+        this.y = y;
+        this.radius = radius;
+        this.speedY=0;
+        this.speedx=5
+        this.board = Board;
+
+        board.ball=this;
+        this.kind ="circle";
+    }
+
+
+})();
+
+
 
 
 (function(){
@@ -62,20 +81,35 @@ self.Bar.prototype ={
     }
 
     self.BoardView.prototype={
+        clean: function(){
+            this.ctx.clearRect(0,0,this.board.width,this.board.height);
+        },
+
         draw: function(){
             for (var i = this.board.elements.length-1; i>=0; i--) {
                 var el = this.board.elements[i];
               
                 draw(this.ctx,el)
             };
-        }
+        },
+
+        
+    play: function(){
+        this.clean();
+        this.draw();   
+       }
     };
     function draw(ctx,element){
         switch(element.kind){
             case "rectangle":
             ctx.fillRect(element.x,element.y,element.width,element.height);
             break;
-            
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x,element.y,element.radius,0,7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
     }
     
@@ -88,6 +122,7 @@ var bar = new Bar(20,100,40,100,board);
 var bar2 = new Bar(700,100,40,100,board);
 var canvas = document.getElementById('canvas');
 var boardview = new BoardView(canvas,board)
+var ball = new Ball(350,100,10,board);
 
 
 
@@ -116,6 +151,6 @@ ev.preventDefault();
 window.requestAnimationFrame(controller);
 
 function controller(){
-    boardview.draw();
+    boardview.play();
     window.requestAnimationFrame(controller);
 };
